4d82b3dea641ea142fce47326387f37bd09f10f9
wishwater <NikitaOgranchuck@gmail.com>
2018-03-10 14:23:46 +0200

    upload_files has been fixed

--- app.py
+++ app.py
@@ -375,22 +375,22 @@ def registr_group():
 def index():
     return render_template('upload.html')
 
-#@app.route("/upload", methods=['POST'])
-#def upload():
- #   target = os.path.join(APP_ROOT, 'images/')
- #   print(target)
- #   if not os.path.isdir(target):
- #       os.mkdir(target)
+@app.route("/upload", methods=['POST'])
+def upload():
+    target = os.path.join(APP_ROOT, 'images/')
+    print(target)
+    if not os.path.isdir(target):
+        os.mkdir(target)
 
-#    for file in request.files.getlist("file"):
- #       print(file)
-  #      filename = file.filename
-   #     destination = "/".join([target, filename])
-    #    print(destination)
-     #   print(filename)
-      #  file.save(destination)
+    for file in request.files.getlist("file"):
+        print(file)
+        filename = file.filename
+        destination = "/".join([target, filename])
+        print(destination)
+        print(filename)
+        file.save(destination)
 
-   # return render_template("complete.html" , image_name = filename)
+    return render_template("complete.html" , image_name = filename)
 
 @app.route('/upload/<filename>')
 def send_image(filename):
